eb66d34c5b8e5b604fb764579f45ea2496d213d4
y-lime <y.murai.tech@gmail.com>
2021-04-14 21:47:10 +0900

    hervenivon/wildrydes-siteのレポジトリをコピー

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_JTi25cDVB',
+        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
+        region: 'eu-west-1'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
