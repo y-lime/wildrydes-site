a37246a148c1bcd08329e552216f51ca2b45b0ce
y-lime <y.murai.tech@gmail.com>
2021-05-17 22:18:48 +0900

    add: cognito configuration

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'ap-northeast-1_QPOJq8xO5',
+        userPoolClientId: '68rpo008coka3ni8bd7tr5rknb',
+        region: 'ap-northeast-1'
     },
     api: {
-        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: ''
     }
 };
